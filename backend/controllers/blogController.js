d71c672b0339d91569d42b9e911e83c3d0748113
GreatnessGrace <graceunstoppable7@gmail.com>
2025-04-28 12:39:12 +0530

    blogController

---
+++ backend/controllers/blogController.js
@@ -0,0 +1,42 @@
+const Blog = require('../models/Blog');
+
+exports.getBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find().populate('author', 'name');
+    res.json(blogs);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.createBlog = async (req, res) => {
+  const { title, content } = req.body;
+  try {
+    const blog = await Blog.create({
+      title,
+      content,
+      author: req.user.id,
+    });
+    res.status(201).json(blog);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(blog);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deleteBlog = async (req, res) => {
+  try {
+    await Blog.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Blog deleted' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
