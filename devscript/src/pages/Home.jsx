91d314f9b57cdc6b004acb617eeebc159f964014
GreatnessGrace <graceunstoppable7@gmail.com>
2025-04-28 16:16:01 +0530

    Notification in case of Home

--- devscript/src/pages/Home.jsx
+++ devscript/src/pages/Home.jsx
@@ -1,20 +1,33 @@
 import { useEffect, useState } from 'react';
 import axios from '../api/axios';
 import BlogCard from '../components/BlogCard';
+import { toast, Toaster } from 'react-hot-toast';
 
 const Home = () => {
   const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
     const fetchBlogs = async () => {
-      const { data } = await axios.get('/blogs');
-      setBlogs(data);
+      try {
+        const { data } = await axios.get('/blogs');
+        setBlogs(data);
+  
+      } catch (error) {
+
+        if (error.response && error.response.data.message == 'No token') {
+          toast.error('Please login to view blogs'); 
+        } else {
+          toast.error('Failed to fetch blogs');
+        }
+      }
     };
     fetchBlogs();
   }, []);
 
   return (
     <div className="min-h-screen bg-gray-50 p-6">
+            <Toaster position="top-right" reverseOrder={false} />
+
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {blogs.map(blog => (
           <BlogCard key={blog._id} blog={blog} />
@@ -23,6 +36,5 @@ const Home = () => {
     </div>
   );
 };
-
 
 export default Home;
