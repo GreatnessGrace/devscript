13c3ae7a4c55a85649030df36210f59e6b1778b9
GreatnessGrace <graceunstoppable7@gmail.com>
2025-04-28 12:45:14 +0530

    Routing

---
+++ devscript/src/App.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import AdminDashboard from './pages/AdminDashboard';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
